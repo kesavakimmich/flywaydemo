3725b8b62f6d4d2bb719e243072665888febffc8
Siva Kesava <sivakesavakuchi@licious.com>
2022-12-11 09:08:07 +0530

    14 commit

---
+++ src/main/java/com/example/flywaydemo/controllers/AttributesController.java
@@ -0,0 +1,15 @@
+package com.example.flywaydemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("attributes")
+public class AttributesController {
+
+    @GetMapping
+    public String getAttributes() {
+        return "Hello World";
+    }
+}
